8a5f8f21f95151699bf07eeff2158621ac806c70
halju <59658500+halju@users.noreply.github.com>
2020-01-29 19:14:21 -0500

    lab3

--- lab3/index.js
+++ lab3/index.js
@@ -3,10 +3,10 @@ const App = Express();
 const port = 8080;
 
 App.get("/", function(req, res) {
-    res.send("<p>Hello world!</p>");
+    res.send("Hello world!");
 });
 
-App.use(Express.static("/public"));
+App.use("/public", Express.static("public"));
 
 App.listen(port, function() {
     console.log("Server running!");
