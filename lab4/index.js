93769c5fb81695bf5027da255de99f4a1b13896b
halju <59658500+halju@users.noreply.github.com>
2020-02-03 18:31:19 -0500

    lab4

--- lab4/index.js
+++ lab4/index.js
@@ -1,14 +1,18 @@
 const Express = require("express");
+//const pokemon = require("json-pokemon");
+const chalk = require("chalk");
 const App = Express();
 const port = 80;
 
-App.get("/:id", req, res) => {
+App.get("/:id/:name", function(req, res) {
     res.send(req.params);
 });
 
-App.listen(port, () => {
-    console.log("Server running!");
+App.listen(port, function() {
+    console.log(chalk.green("Server running!"));
 });
 
 // "/id/#id"
 // "/name/#name"
+
+//req.params.id  req.params.name
