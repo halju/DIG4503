d1379437b23001f5d11484da5f53f269d3638743
halju <59658500+halju@users.noreply.github.com>
2020-02-05 18:19:19 -0500

    lab4

---
+++ lab4/Pokemon.js
@@ -0,0 +1,7 @@
+class Pokemon {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+module.exports = Pokemon;
--- lab4/index.js
+++ lab4/index.js
@@ -1,18 +1,45 @@
 const Express = require("express");
-//const pokemon = require("json-pokemon");
-const chalk = require("chalk");
 const App = Express();
 const port = 80;
+const chalk = require("chalk");
+const Pokemon = require ("./Pokemon.js");
+const getPokemon = require('json-pokemon/getPokemon');
 
-App.get("/:id/:name", function(req, res) {
-    res.send(req.params);
+const pokemons = getPokemon();
+const id = getPokemon.getPokemonById(1);
+const name = getPokemon.getPokemonByName('Bulbasaur');
+pokemons.push(new Pokemon(id, name));
+
+App.get("/id/:id", function(req, res) {
+    let result = {"error": "Could not find!"}
+    pokemons.forEach(value, function() {
+        if(value.id == req.params.id) {
+            result = value;
+        }
+    });
+    if(result.error) {
+        console.log(chalk.red(req.path));
+    } else {
+        console.log(chalk.green(req.path));
+    }
+    res.send(result);
+});
+
+App.get("/name/:name", function(req, res) {
+    let result = {"error": "Could not find!"}
+    pokemons.forEach(value, function() {
+        if(value.name == req.params.name) {
+            result = value;
+        }
+    });
+    if(result.error) {
+        console.log(chalk.red(req.path));
+    } else {
+        console.log(chalk.green(req.path));
+    }
+    res.send(result);
 });
 
 App.listen(port, function() {
     console.log(chalk.green("Server running!"));
 });
-
-// "/id/#id"
-// "/name/#name"
-
-//req.params.id  req.params.name
