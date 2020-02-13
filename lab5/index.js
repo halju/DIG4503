d3a49f6974f29660a72aa32914c55b84b6ec1ce4
halju <59658500+halju@users.noreply.github.com>
2020-02-12 19:07:16 -0500

    lab5

---
+++ lab5/index.js
@@ -0,0 +1,43 @@
+const Express = require("express");
+const App = Express();
+const port = 80;
+
+const chalk = require("chalk");
+const pokemons = require('json-pokemon');
+
+App.use(Express.json());
+App.use("/", Express.static("public"));
+
+App.get("/id/:id", function(req, res) {
+    let result = {"error": "Could not find!"}
+    pokemons.forEach(function(value) {
+        if(value.id == req.params.id) {
+            result = value;
+        }
+    });
+    if(result.error) {
+        console.log(chalk.red(req.path));
+    } else {
+        console.log(chalk.green(req.path));
+    }
+    res.send(result);
+});
+
+App.get("/name/:name", function(req, res) {
+    let result = {"error": "Could not find!"}
+    pokemons.forEach(function(value) {
+        if(value.name == req.params.name) {
+            result = value;
+        }
+    });
+    if(result.error) {
+        console.log(chalk.red(req.path));
+    } else {
+        console.log(chalk.green(req.path));
+    }
+    res.send(result);
+});
+
+App.listen(port, function() {
+    console.log(chalk.green("Server running!"));
+});
