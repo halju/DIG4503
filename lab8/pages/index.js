ef841ddb09b760c2c6a15ef06a9ee66cb04c9095
mgsotacon <halthehusky@gmail.com>
2020-03-21 14:55:31 -0400

    lab8

--- lab8/pages/index.js
+++ lab8/pages/index.js
@@ -1,3 +1,45 @@
-import Head from 'next/head'
+class Home extends React.Component {
 
-export default Home
+    getName() {
+      let name = document.querySelector("#nameInput");
+      fetch("/api/pokemon/name/" + name.value)
+      .then((res) => { return res.json(); } )
+      .then((processed) => {
+        let resultElement = document.querySelector("#results");
+        if(processed.error) {
+          resultElement.innerHTML = "Could not find this pokemon!";
+        } else {
+          resultElement.innerHTML = "This pokemon's ID is " + processed.id;
+        }
+      });
+    }
+
+    getId() {
+        let id = document.querySelector("idInput");
+        fetch("/api/pokemon/id/" + id.value)
+        .then((res) => { return res.json(); } )
+        .then((processed) => {
+          let resultElement = document.querySelector("#results2");
+          if(processed.error) {
+            resultElement.innerHTML = "Could not find this pokemon!";
+          } else {
+            resultElement.innerHTML = "This pokemon's name is " + processed.name;
+          }
+        });
+      }
+  
+    render() {
+      return (
+        <div>
+          <input type="text" id="nameInput" />
+          <button onClick={() => { this.getName() } }>search</button>
+          <div id="results"></div>
+          <input type="text" id="idInput" />
+          <button onClick={() => { this.getId() } }>search</button>
+          <div id="results2"></div>
+        </div>
+      );
+    }
+  }
+  
+  export default Home;
