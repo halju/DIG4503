9511524ac8f64a6d98adf02144523ed394aff922
mgsotacon <halthehusky@gmail.com>
2020-03-21 15:15:06 -0400

    lab8

--- lab8/pages/index.js
+++ lab8/pages/index.js
@@ -1,45 +1,45 @@
 class Home extends React.Component {
 
-    getName() {
-      let name = document.querySelector("#nameInput");
-      fetch("/api/pokemon/name/" + name.value)
+  getName() {
+    let name = document.querySelector("#nameInput");
+    fetch("/api/pokemon/name/" + name.value)
+    .then((res) => { return res.json(); } )
+    .then((processed) => {
+      let resultElement = document.querySelector("#results");
+      if(processed.error) {
+        resultElement.innerHTML = "Could not find this pokemon!";
+      } else {
+        resultElement.innerHTML = "This pokemon's ID is " + processed.id;
+      }
+    });
+  }
+
+  getId() {
+      let id = document.querySelector("idInput");
+      fetch("/api/pokemon/id/" + id.value)
       .then((res) => { return res.json(); } )
       .then((processed) => {
-        let resultElement = document.querySelector("#results");
+        let resultElement = document.querySelector("#results2");
         if(processed.error) {
           resultElement.innerHTML = "Could not find this pokemon!";
         } else {
-          resultElement.innerHTML = "This pokemon's ID is " + processed.id;
+          resultElement.innerHTML = "This pokemon's name is " + processed.name;
         }
       });
     }
 
-    getId() {
-        let id = document.querySelector("idInput");
-        fetch("/api/pokemon/id/" + id.value)
-        .then((res) => { return res.json(); } )
-        .then((processed) => {
-          let resultElement = document.querySelector("#results2");
-          if(processed.error) {
-            resultElement.innerHTML = "Could not find this pokemon!";
-          } else {
-            resultElement.innerHTML = "This pokemon's name is " + processed.name;
-          }
-        });
-      }
-  
-    render() {
-      return (
-        <div>
-        <input type="text" id="nameInput" />
-        <button onClick={() => { this.getName() } }>Search</button>
-        <div id="results"></div>
-        <input type="text" id="idInput" />
-        <button onClick={() => { this.getId() } }>Search</button>
-        <div id="results2"></div>
-      </div>
-      );
-    }
+  render() {
+    return (
+      <div>
+      <input type="text" id="nameInput" />
+      <button onClick={() => { this.getName() } }>Search</button>
+      <div id="results"></div>
+      <input type="text" id="idInput" />
+      <button onClick={() => { this.getId() } }>Search</button>
+      <div id="results2"></div>
+    </div>
+    );
   }
-  
-  export default Home;
+}
+
+export default Home;
