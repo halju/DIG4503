4fed6d0115c546718d2f6c58982a4bbfb8642f47
mgsotacon <halthehusky@gmail.com>
2020-03-21 00:15:16 -0400

    lab8

---
+++ lab8/components/NameSearch/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+class NameSearch extends React.Component {
+  NameSearch(event) {
+    event.preventDefault();
+    let element = document.querySelector("#pokemonName");
+    fetch("http://localhost:80/name/" + element.value).then((res) => {
+        return res.json();
+    }).then((processed) => {
+        let reporting = document.querySelector("#reportingArea");
+        if(processed.error) {
+            reporting.innerHTML = processed.error;
+        } else {
+            reporting.innerHTML = processed.name;
+        }
+    });
+}
+  render() {
+    return (
+      <div className="form">
+        <form onSubmit={this.NameSearch}>
+          <label htmlFor="pokemonName">Pokemon Name:&nbsp;</label>
+          <input id="pokemonName" type="text" />&nbsp;
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  } 
+}
+
+export default NameSearch;
